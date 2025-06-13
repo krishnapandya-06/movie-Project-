5a971a8ea18654ba8b368c9217d9246bc2e4f1f3
krishnapandya-06 <pandyakrishna373@gmail.com>
2025-06-13 12:41:13 +0530

    3rd Commit

--- app.js
+++ app.js
@@ -5,21 +5,16 @@ const movieRoutes = require('./routes/movies');
 const indexRoutes = require('./routes/index');
 const app = express();
 
-// MongoDB Config
 require('./config/database');
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// View Engine
 app.set('view engine', 'ejs');
 
-// Routes
 app.use('/', indexRoutes);
 app.use('/', movieRoutes);
 
-// Server
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
